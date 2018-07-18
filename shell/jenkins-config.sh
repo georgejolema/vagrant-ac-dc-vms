03f5b124f5029a5a1f3a1cff9e4c42213146476c
jorge <georgejolema@gmail.com>
2018-07-17 23:18:35 -0600

    add nodejs configurations

--- shell/jenkins-config.sh
+++ shell/jenkins-config.sh
@@ -2,3 +2,6 @@ wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key 
 sudo echo "deb https://pkg.jenkins.io/debian-stable binary/" >> /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install -y jenkins
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo ln -s /usr/bin/nodejs /usr/bin/node
